c4532ee5322f6b44be72e88a5063761e1caf0574
Indervir Singh Banipal <indervir1988@gmail.com>
2017-10-15 17:29:26 -0500

    Pushing visual stuff

---
+++ testApp/FBUpdateImages.py
@@ -0,0 +1,42 @@
+from FBDb import *
+import props
+
+db_client = FBDb.connect(props.db_host, props.db_port)
+
+cursor = FBDb.get_cursor(db_client, "buet3")
+for person in cursor:
+    for profile in person["profiles"]:
+        profile
+
+
+
+
+
+
+
+
+
+
+# db_client.facebook_db.buet3.update(
+#     {"_id": person["_id"]},
+#     {
+#         "person": person["person"],
+#         "profiles": person["profiles"]
+#     }
+# )
+
+# dict = {}
+# dict["score"] = 0
+# dict["score1"] = 0
+# dict["score2"] = 0
+# dict["profile"] = "https://www.facebook.com/DrIftekharAnam/"
+# dict["actual"] = "yes"
+# dict["friends"] = []
+# dict["name"] = "Dr. Iftekhar Anam"
+# dict["pic"] = "https://scontent-dft4-2.xx.fbcdn.net/v/t1.0-9/12741859_829622840482257_6991609422952546545_n.jpg?oh=02cfd6f06369185ac4446b23595d6ed4&oe=58C7DAEB"
+# dict["education"] = "Dr. Iftekhar Anam joined University of Asia Pacific (UAP) in 2000 with an excellent academic background, including honors like the Chancellors Awards for S.S.C. and H.S.C. results, University Gold Medal and Malik Akram Hossain (CE departmental) Gold medal at BUET and the highest distinctions in his post-graduate studies at University of Texas and Texas A&M University.Before joining UAP, he worked as a lecturer at BUET, Teaching and Research Assistant at The University Texas at Austin and Texas A&M University"
+# dict["location"] = ""
+# dict["work"] = "Porfessor, Dept. of Civil Engineering, University of Asia Pacific (This is a FAN PAGE, so dont get confused. This Page is run by his students.)"
+
+
+
