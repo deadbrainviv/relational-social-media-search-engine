b34caabad779e93ec2abba3b538a848356a90728
Indervir Singh Banipal <indervir1988@gmail.com>
2018-02-28 02:57:21 -0600

    Adding refresh db and server

---
+++ refresh_db_and_server.sh
@@ -0,0 +1,18 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        sudo service mongod stop
+        sudo service mongod start
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew services stop mongodb
+        brew services start mongodb
+fi
+echo "MongoDB RE-started..."
+output=$(ps aux | grep "manage.py runserver" | awk '{print$2}')
+for line in $output
+do
+        echo "Going to kill process: $line"  
+        kill -9 $line
+done
+echo "Starting the server.."
+python manage.py runserver 0:8000 &
+exit 0
+
